38732a25eceef82b1032d4c27e5b37e42813ec5c
jsmithnh09 <jsmithnh09@gmail.com>
2023-07-23 19:21:40 -0400

    Reducing complexity.

--- days/07/main.jl
+++ days/07/main.jl
@@ -1,120 +1,52 @@
-mutable struct Directory
-    name::String
-    files::Dict{String, UInt64}
-    subdirs::Dict{String, Directory}
-end
 
 ### constants and type-aliases
-const File = Dict{String, UInt64}
 const MAX_DIRSIZE = 100000
+const File = Dict{String, Int}
 const LIST_CMD = "\$ ls"
 const RE_FILE = r"(\d+) (.*)"
 
-Directory(name) = Directory(name, File(), Dict{String, Directory}())
-
-function addfile!(d::Directory, name::String, fsize::Integer)
-    d.files[name] = UInt64(fsize)
-    d
-end
-
-function adddir!(d::Directory, name::String)
-    d.subdirs[name] = Directory(name)
-end
-
-function parsefile(instr::String, d::File)
-    m = match(RE_FILE, instr)
-    d[m[2]] = parse(UInt64, m[1])
-end
-
-"""
-    filesum(d, full = false)
-
-Filesum will determine the sum of the filesizes for the Directory
-`d`. If `full` is true, the recursive size is calculated.
-"""
-function filesum(d::Directory; full::Bool = false)
-    sumval = 0
-    for filename ∈ keys(d.files)
-        sumval += d.files[filename]
-    end
-    if (full)
-        for key in keys(d.subdirs)
-            sumval += filesum(d.subdirs[key], full = true)
-        end
-    end
-    Int(sumval)
-end
-filesum(f::File) = Int(sum(values(f)))
-
-
-function sizesum(d::Directory; cursum::Int = 0)
-    size_check = 0
-    for dname ∈ keys(d.subdirs)
-        size_check = filesum(d.subdirs[dname], full = true)
-        if (size_check <= MAX_DIRSIZE)
-            cursum += size_check
-            cursum += sizesum(d.subdirs[dname])
-        end
-    end
-    cursum
-end
-
 function parsefile(instr::String)
-    !isfile(instr) && throw(ArgumentError("Not a valid filepath."))
-    root = Directory("/")
-    node = root
-    trace = ["/"]
+    !isfile(instr) && throw(ArgumentError("not a valid filepath."))
+    fsizes = File()
+    trace = []
     open(instr, "r") do io
-        while !eof(io)
-            line = readline(io)
-            if isempty(line)
-                break
-            end
-            if line == "\$ cd /" # root node.
+        lines = readlines(io)
+        for line ∈ lines
+            if startswith(line, LIST_CMD) || startswith(line, "dir")
                 continue
-            elseif line == LIST_CMD # listing node.
-                line = readline(io)
-                while line[1] != '$' # listing directories/files.
-                    if line[1:3] == "dir"
-                        newfold = split(line)[2]
-                        adddir!(node, String(newfold))
-                    elseif !isempty(match(RE_FILE, line))
-                        m = match(RE_FILE, line)
-                        addfile!(node, String(m[2]), parse(UInt64, m[1]))
-                    end
-                    line = readline(io)
-                    if isempty(line)
-                        break
-                    end
-                end
             end
-            # if we're exiting the list command, check
-            if isempty(line)
-                break
-            elseif line[1:4] == "\$ cd"
-                newfold = String(split(line)[end])
-                if newfold == ".."
+            if startswith(line, "\$ cd")
+                destdir = String(split(line)[3]) # $ is the first element of the split.
+                if destdir == ".."
                     _ = pop!(trace)
-                    node = root
-                    for fold ∈ trace[2:end]
-                        node = node.subdirs[fold]
-                    end
                 else
-                    node = node.subdirs[newfold]
-                    push!(trace, newfold)
+                    path = !isempty(trace) ? "$(trace[end])_$destdir" : "$destdir"
+                    push!(trace, path)
+                end
+            else
+                (size, _) = split(line, " ")
+                for path ∈ trace
+                    if !haskey(fsizes, path)
+                        fsizes[path] = parse(Int, size)
+                    else
+                        fsizes[path] += parse(Int, size)
+                    end
                 end
             end
         end
     end
-    root
+    fsizes
 end
 
-                        
+# sum of the files part I.
+parsesum(files::File) = sum(x for x in values(files) if x <= MAX_DIRSIZE)
 
+# smallest size needed to satisfy argmin(Dtotal - fpath >= 300000)
+minsize(files::File) = 30000000 - (70000000 - files["/"])
 
-
-
-                            
-
-            
-
+# part II
+function parse_minsum(files::File)
+    Msize = minsize(files)
+    svals =  values(files) |> collect |> sort
+    svals[findfirst(svals .> Msize)]
+end
--- days/07/tests.jl
+++ days/07/tests.jl
@@ -5,8 +5,5 @@ include("main.jl")
 node = parsefile("example.txt")
 
 @testset "total size" begin
-    @test filesum(node, full=true) == 48381165
-    @test filesum(node.subdirs["a"], full=true) == 94853
-    @test filesum(node.subdirs["d"], full=true) == 24933642
-    @test sizesum(node) == 95437
+    @test parsesum(node) == 95437
 end
